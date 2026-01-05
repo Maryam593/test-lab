2168c8d3fac2210f3575adf2366182cbee15cdf3
Maryam Saba <maryams91101@gmail.com>
2026-01-05 20:55:58 +0500

    code cleanup

--- test-next-lab/app/(main)/about/page.tsx
+++ test-next-lab/app/(main)/about/page.tsx
@@ -9,36 +9,7 @@ const AboutPage = () => {
    <h1 className="bg-green-700">How it Works</h1>
    <span className=" bg-amber-500">Your Financial Investment Solution.</span>
     </div>
-    {/* <div className="flex justify-between flex-row w-[50%]">
-        <div className="bg-red-600"><h1>How PropStake
-Works</h1>
-<p>Invest in curated real estate opportunities through a simple, transparent process. From account setup to returns tracking, everything is designed to be easy and secure.</p>
-<ParticleButton>Get Started</ParticleButton>
-</div>
-        <div className="bg-blue-800 w-[50%]">
-            <div>
-                <span>01</span>
-                <p><h1>Explore Investments</h1>
-                <br />Browse curated property opportunities with data and visuals.</p>
-            </div>
-             <div>
-                <span>02</span>
-                <p><h1>Explore Investments</h1>
-                <br />Browse curated property opportunities with data and visuals.</p>
-            </div>
-             <div>
-                <span>03</span>
-                <p><h1>Explore Investments</h1>
-                <br />Browse curated property opportunities with data and visuals.</p>
-            </div>
-             <div>
-                <span>04</span>
-                <p><h1>Explore Investments</h1>
-                <br />Browse curated property opportunities with data and visuals.</p>
-            </div>
-        </div>
 
-    </div> */}
     <div className="grid bg-fuchsia-700 grid-cols-2 gap-4 p-4">
        <div>
         <h1>How PropStake
