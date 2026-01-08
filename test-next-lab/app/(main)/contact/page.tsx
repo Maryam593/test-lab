bae8a03880e203ef5d95c0dad378645e94f96d3b
Maryam Saba <maryams91101@gmail.com>
2026-01-08 23:14:08 +0500

    setting up contact page

---
+++ test-next-lab/app/(main)/contact/page.tsx
@@ -0,0 +1,12 @@
+const contactPage = () => {
+  return (
+    <>
+    <div>
+        <h1>Get in Touch</h1>
+        <p>Have a question or need assistance? Send us a message and we'll get back to you soon.</p>
+    </div>
+    </>
+  )
+}   
+
+export default contactPage;
