cfe6f13e972413fd0816642c25a5be1a7bc4024f
Maryam Saba <maryams91101@gmail.com>
2026-01-11 23:50:01 +0500

    setting up ui of pages

--- test-next-lab/app/(auth)/forgetPassword/page.tsx
+++ test-next-lab/app/(auth)/forgetPassword/page.tsx
@@ -3,11 +3,11 @@
 import MouseEffectCard from "@/components/kokonutui/mouse-effect-card";
 
 const ForgetPassword = () => {
-  return <div>
+  return <div className="flex min-h-screen  bg-black flex-col items-center justify-center  dark:bg-black">
  <MouseEffectCard title="Reset your password" footerText="Remembered your password? Login here" primaryCtaText="Reset Password" primaryCtaUrl="/login">
   <form action="post">
-    <input type="password" name="oldPassword" id="oldPassword" placeholder="Enter your old password" />
-    <input type="password" name="newPassword" id="newPassword" placeholder="Enter your new password" />
+    <input type="password" name="oldPassword" id="oldPassword" placeholder="Enter your old password" className=" bg-black text-white rounded-2xl pl-5 pt-2 pb-1" />
+    <input type="password" name="newPassword" id="newPassword" placeholder="Enter your new password" className=" bg-black text-white rounded-2xl pl-5 pt-2 pb-1" />
   </form>
   </MouseEffectCard>
   </div>;
--- test-next-lab/app/(auth)/signup/page.tsx
+++ test-next-lab/app/(auth)/signup/page.tsx
@@ -4,17 +4,17 @@ import MouseEffectCard from "@/components/kokonutui/mouse-effect-card";
 
 export default function SignUpPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black dark:bg-black">
       <MouseEffectCard title="Create an account" footerText="Not a member? Sign up here" primaryCtaText="Sign Up" primaryCtaUrl="/l">
         <form action="post">
           <div className="flex flex-row justify-between">
-            <input type="text" name="fname" id="fname" placeholder="enter your first name" />
-            <input type="text" name="lname" id="lname" placeholder="enter your last name" />
+            <input type="text" className=" bg-black text-white rounded-2xl pl-5 pt-2 pb-1" name="fname" id="fname" placeholder="enter your first name" />
+            <input type="text" className=" bg-black text-white rounded-2xl pl-5 pt-2 pb-1" name="lname" id="lname" placeholder="enter your last name" />
           </div>
           <input type="email" className=" bg-black text-white rounded-2xl pl-5 pt-2 pb-1" placeholder="enter you email"></input>
           <div className="flex flex-row gap-2 mt-3 ml-3">
-            <input type="password" name="password" id="password" placeholder="enter your password" />
-            <input type="checkbox" name="forgetP" id="forgetP" />
+            <input type="password" className = " bg-black text-white rounded-2xl pl-5 pt-2 pb-1" name="password" id="password" placeholder="enter your password" />
+            <input type="checkbox" className = " bg-black text-white rounded-2xl pl-5 pt-2 pb-1" name="forgetP" id="forgetP" />
             <a className="auth/forgetPassword">Forget Password?</a>
           </div>
         </form>
